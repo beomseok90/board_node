93c1adeb6a6c42184ead770f748835bfcd6eb2d0
beomseok <beom8776@naver.com>
2020-11-03 17:26:55 +0900

    게시판 초기 설정

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello World");
+// DB settings
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function(){
+    console.log("DB connected");
 });
+db.once("error", function(err){
+    console.log("DB ERROR : ", err);
+});
+
+// Other settings
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+// Routes
+app.use("/", require("./routes/home"));
 
 var port = 3000;
 app.listen(port, function(){
