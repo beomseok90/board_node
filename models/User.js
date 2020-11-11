ce49c9d4b6dd992e83b6c5a1e16420b08b468d65
beomseok <beom8776@naver.com>
2020-11-11 17:47:14 +0900

    Users validation check 추가

--- models/User.js
+++ models/User.js
@@ -1,44 +1,68 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var bcrypt = require("bcryptjs");
 
 // schema
 var userSchema = mongoose.Schema({
-  username:{type:String, required:[true,'Username is required!'], unique:true},
-  password:{type:String, required:[true,'Password is required!'], select:false},
-  name:{type:String, required:[true,'Name is required!']},
-  email:{type:String}
+  username:{
+    type:String,
+    required:[true,"Username is required!"],
+    match:[/^.{4,12}$/,"Should be 4-12 characters!"],
+    trim:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:[true,"Password is required!"],
+    select:false
+  },
+  name:{
+    type:String,
+    required:[true,"Name is required!"],
+    match:[/^.{4,12}$/,"Should be 4-12 characters!"],
+    trim:true
+  },
+  email:{
+    type:String,
+    match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,"Should be a vaild email address!"],
+    trim:true
+  }
 },{
   toObject:{virtuals:true}
 });
 
 // virtuals
-userSchema.virtual('passwordConfirmation')
+userSchema.virtual("passwordConfirmation")
   .get(function(){ return this._passwordConfirmation; })
   .set(function(value){ this._passwordConfirmation=value; });
 
-userSchema.virtual('originalPassword')
+userSchema.virtual("originalPassword")
   .get(function(){ return this._originalPassword; })
   .set(function(value){ this._originalPassword=value; });
 
-userSchema.virtual('currentPassword')
+userSchema.virtual("currentPassword")
   .get(function(){ return this._currentPassword; })
   .set(function(value){ this._currentPassword=value; });
 
-userSchema.virtual('newPassword')
+userSchema.virtual("newPassword")
   .get(function(){ return this._newPassword; })
   .set(function(value){ this._newPassword=value; });
 
 // password validation
+var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+var passwordRegexErrorMessage = 'Should be minimum 8 characters of alphabet and number combination!';
 userSchema.path('password').validate(function(v) {
   var user = this;
-
+  
   // create user
   if(user.isNew){
     if(!user.passwordConfirmation){
-      user.invalidate('passwordConfirmation', 'Password Confirmation is required.');
+      user.invalidate("passwordConfirmation", "Password Confirmation is required.");
     }
 
-    if(user.password !== user.passwordConfirmation) {
+    if(!passwordRegex.test(user.password)){
+      user.invalidate('password', passwordRegexErrorMessage);
+    }
+    else if(user.password !== user.passwordConfirmation) {
       user.invalidate('passwordConfirmation', 'Password Confirmation does not matched!');
     }
   }
@@ -46,22 +70,25 @@ userSchema.path('password').validate(function(v) {
   // update user
   if(!user.isNew){
     if(!user.currentPassword){
-      user.invalidate('currentPassword', 'Current Password is required!');
+      user.invalidate("currentPassword", "Current Password is required!");
     }
     else if(!bcrypt.compareSync(user.currentPassword, user.originalPassword)){ // 2
-      user.invalidate('currentPassword', 'Current Password is invalid!');
+      user.invalidate("currentPassword", "Current Password is invalid!");
     }
 
-    if(user.newPassword !== user.passwordConfirmation) {
+    if(user.newPassword && !passwordRegex.test(user.newPassword)){
+      user.invalidate("newPassword", passwordRegexErrorMessage);
+    }
+    else if(user.newPassword !== user.passwordConfirmation) {
       user.invalidate('passwordConfirmation', 'Password Confirmation does not matched!');
     }
   }
 });
 
 // hash password
-userSchema.pre('save', function (next){
+userSchema.pre("save", function (next){
   var user = this;
-  if(!user.isModified('password')){
+  if(!user.isModified("password")){
     return next();
   }
   else {
@@ -77,5 +104,5 @@ userSchema.methods.authenticate = function (password) {
 };
 
 // model & export
-var User = mongoose.model('user',userSchema);
+var User = mongoose.model("user",userSchema);
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
 
-// Index // 1
+// Index
 router.get('/', function(req, res){
   User.find({})
     .sort({username:1})
@@ -14,16 +14,23 @@ router.get('/', function(req, res){
  
 // New
 router.get('/new', function(req, res){
-  res.render('users/new');
+  var user = req.flash('user')[0] || {};
+  var errors = req.flash('errors')[0] || {};
+  res.render('users/new', { user:user, errors:errors });
 });
 
 // create
 router.post('/', function(req, res){
   User.create(req.body, function(err, user){
-    if(err) return res.json(err);
+    if(err){
+      req.flash('user', req.body);
+      req.flash('errors', parseError(err));
+      return res.redirect('/users/new');
+    }
     res.redirect('/users');
   });
 });
+
 
 // show
 router.get('/:username', function(req, res){
@@ -35,29 +42,40 @@ router.get('/:username', function(req, res){
 
 // edit
 router.get('/:username/edit', function(req, res){
-  User.findOne({username:req.params.username}, function(err, user){
-    if(err) return res.json(err);
-    res.render('users/edit', {user:user});
-  });
+  var user = req.flash('user')[0];
+  var errors = req.flash('errors')[0] || {};
+  if(!user){
+    User.findOne({username:req.params.username}, function(err, user){
+      if(err) return res.json(err);
+      res.render('users/edit', { username:req.params.username, user:user, errors:errors });
+    });
+  }
+  else {
+    res.render('users/edit', { username:req.params.username, user:user, errors:errors });
+  }
 });
 
-// update // 2
+// update
 router.put('/:username', function(req, res, next){
-  User.findOne({username:req.params.username}) // 2-1
-    .select('password') // 2-2
+  User.findOne({username:req.params.username})
+    .select('password')
     .exec(function(err, user){
       if(err) return res.json(err);
 
       // update user object
       user.originalPassword = user.password;
-      user.password = req.body.newPassword? req.body.newPassword : user.password; // 2-3
-      for(var p in req.body){ // 2-4
+      user.password = req.body.newPassword? req.body.newPassword : user.password;
+      for(var p in req.body){
         user[p] = req.body[p];
       }
 
       // save updated user
       user.save(function(err, user){
-        if(err) return res.json(err);
+        if(err){
+          req.flash('user', req.body);
+          req.flash('errors', parseError(err));
+          return res.redirect('/users/'+req.params.username+'/edit');
+        }
         res.redirect('/users/'+user.username);
       });
   });
@@ -72,3 +90,23 @@ router.delete('/:username', function(req, res){
 });
 
 module.exports = router;
+
+module.exports = router;
+
+// functions
+function parseError(errors){
+  var parsed = {};
+  if(errors.name == 'ValidationError'){
+    for(var name in errors.errors){
+      var validationError = errors.errors[name];
+      parsed[name] = { message:validationError.message };
+    }
+  }
+  else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
+    parsed.username = { message:'This username already exists!' };
+  }
+  else {
+    parsed.unhandled = JSON.stringify(errors);
+  }
+  return parsed;
+}
