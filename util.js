cf43cce96c33ef5bc96feb3737ba1505264b0ce3
beomseok <beom8776@naver.com>
2020-11-19 09:41:28 +0900

    임시 저장

---
+++ util.js
@@ -0,0 +1,18 @@
+var util = {};
+
+util.parseError = fucntion(errors){
+    var parsed = {};
+    if(errors.name == "ValidationError"){
+        for(var name in errors.errors){
+            var validationError = errors.errors[name];
+            parsed[name] = {message:validationError.message};
+        }
+    }else if(errors.code == "11000" && errors.errmsg.indexOf("username") > 0) {
+        parsed.username - {message:"This username already exist!"};
+    }else{
+        parsed.unhandled = JSON.stringify(errors);
+    }
+    return parsed;
+}
+
+module.exports = util;
